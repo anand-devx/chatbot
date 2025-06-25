5e39e7b18c5b63c7d4b32d687932e8a660ad2692
anand-dev <anand110206@gmail.com>
2025-06-25 23:31:22 +0530

    fix buffer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
+ build: {
+    commonjsOptions: {
+      include: ['node_modules/buffer/index.js'],
+    },
   },
   resolve: {
     alias: {
-      buffer: 'buffer'
-    }
-  }
+      buffer: 'buffer', // ensure correct path resolution
+    },
+  },
 })
-
 
 
